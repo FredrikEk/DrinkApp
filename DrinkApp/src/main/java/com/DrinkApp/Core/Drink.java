fcd01bd55c628997bb049688f1490df7746e0276
Fredrik Ek <fredrik.ek.s@gmail.com>
2014-10-06 16:53:32 +0200

    Some initial models

---
+++ DrinkApp/src/main/java/com/DrinkApp/Core/Drink.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.DrinkApp.Core;
+
+import com.DrinkApp.persistence.AbstractEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Drink extends AbstractEntity {
+    
+    @ManyToOne
+    private User user;
+    @Column
+    private String name;
+    @ManyToMany
+    private List<Ingredient> ingredients;
+    @OneToMany
+    private List<Step> steps;
+    @ManyToMany
+    private List<Type> types;
+
+    public Drink() {
+        this.user        = new User();
+        this.name        = "";
+        this.ingredients = new ArrayList();
+        this.steps       = new ArrayList();
+        this.types       = new ArrayList();
+    }
+    
+    public Drink(String name, User user, List<Ingredient> ingredients, List<Step> steps, List<Type> types) {
+        this.name        = name;
+        this.user        = user;
+        this.ingredients = ingredients;
+        this.steps       = steps;
+        this.types       = types;
+        }
+    
+    public Drink(Long id, String username, String name, User user, List<Ingredient> ingredients, List<Step> steps, List<Type> types) {
+        super(id);
+        this.name        = name;
+        this.user        = user;
+        this.ingredients = ingredients;
+        this.steps       = steps;
+        this.types       = types;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+    
+    public List<Step> getSteps() {
+        return steps;
+    }
+    
+    public List<Type> getTypes() {
+        return types;
+    }
+    
+    @Override
+    public String toString() {
+        return "Drink{" + "id=" + getId() + ", name=" + name + '}';
+    }
+}
---
+++ DrinkApp/src/main/java/com/DrinkApp/Core/Step.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.DrinkApp.Core;
+
+import com.DrinkApp.persistence.AbstractEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Fredrik
+ */
+@Entity
+public class Step extends AbstractEntity {
+    
+    @Column
+    private int stepNr;
+    @Column
+    private String description;
+    
+    public Step(){
+        this.description = "";
+        this.stepNr = 0;
+     
+    }
+    
+    public Step(String description, int stepNr) {
+        this.description = description;
+        this.stepNr = stepNr;
+        }
+    
+    public Step(Long id, String name) {
+        super(id);
+        this.description = description;
+        this.stepNr = stepNr;
+    }
+    
+    public int getStepNr() {
+        return stepNr;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" + "id=" + getId() + ", stepNr=" + stepNr + ", description=" + description + '}';
+    }
+    
+}
