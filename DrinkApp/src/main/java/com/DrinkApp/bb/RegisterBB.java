c36996db39f938f520a45e1cc705a6656a5d0a3a
Michael <michaeltran_93@hotmail.com>
2014-10-10 22:03:59 +0200

    update

---
+++ DrinkApp/src/main/java/com/DrinkApp/bb/RegisterBB.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.DrinkApp.bb;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author martinkastebo
+ */
+@Named
+@RequestScoped
+public class RegisterBB {
+    
+    @NotNull
+    private String username;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+    @NotNull
+    private String rePassword;
+    
+    @PostConstruct
+    public void post(){
+        LOG.log(Level.INFO, "******alive ");
+    }
+    private static final Logger LOG = Logger.getLogger(RegisterBB.class.getName());
+    
+    
+    public void setUsername(String username){
+        this.username = username;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    public void setRePassword(String rePassword){
+        this.rePassword = rePassword;
+    }
+    
+    public String getUsername(){
+        return username;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+    
+    public String getRePassword(){
+        return rePassword;
+    }
+    
+}
