21fbfa41ec2d702127d4c062da73b8c76735e434
Michael <michaeltran_93@hotmail.com>
2014-10-13 20:33:29 +0200

    reworked authentication, added filters so you should only be able to access certain web pages depending on if you're logged in or not, minor bug fixes and added web pages link access

--- DrinkApp/src/main/java/com/DrinkApp/bb/LoginBB.java
+++ DrinkApp/src/main/java/com/DrinkApp/bb/LoginBB.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.DrinkApp.bb;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import javax.validation.constraints.NotNull;
 
@@ -15,28 +15,37 @@ import javax.validation.constraints.NotNull;
  * @author martinkastebo
  */
 @Named
-@RequestScoped
-public class LoginBB {
-    
+@SessionScoped
+public class LoginBB implements Serializable {
+
     @NotNull
     private String username;
     @NotNull
     private String password;
-    
-    public void setUsername(String username){
+    private boolean loggedIn;
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public void setUsername(String username) {
         this.username = username;
     }
-    
-    public String getUsername(){
+
+    public String getUsername() {
         return username;
     }
-    
-    public void setPassword(String password){
+
+    public void setPassword(String password) {
         this.password = password;
     }
-    
-    public String getPassword(){
+
+    public String getPassword() {
         return password;
     }
-    
+
 }
--- DrinkApp/src/main/java/com/DrinkApp/ctrl/LoginCtrl.java
+++ DrinkApp/src/main/java/com/DrinkApp/ctrl/LoginCtrl.java
@@ -3,11 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.DrinkApp.ctrl;
 
-import com.DrinkApp.auth.AuthDAO;
-import com.DrinkApp.auth.User;
 import com.DrinkApp.bb.LoginBB;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,53 +24,38 @@ import javax.servlet.http.HttpServletRequest;
 @Named
 @RequestScoped
 public class LoginCtrl {
-    
+
     private static final Logger LOG = Logger.getLogger(LoginCtrl.class.getSimpleName());
-    
-    @Inject
-    private AuthDAO authDAO;
+
     private LoginBB lb;
-    
+
     @Inject
-    protected void setLoginBB(LoginBB lb){
+    protected void setLoginBB(LoginBB lb) {
         this.lb = lb;
     }
-    
+
     public String login() {
         FacesContext context = FacesContext.getCurrentInstance();
         ExternalContext externalContext = context.getExternalContext();
         HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
         LOG.log(Level.INFO, "*** Try login {0} {1}", new Object[]{lb.getUsername(), lb.getPassword()});
-        
-        // Really check is there some data in database?
-        User u =  authDAO.find(lb.getUsername());
-        LOG.log(Level.INFO, "*** Found {0} {1}", new Object[]{lb.getUsername(), lb.getPassword()});
-        
-        
         try {
             request.login(lb.getUsername(), lb.getPassword());
             LOG.log(Level.INFO, "*** Login success");
-            LOG.log(Level.INFO, "*** User principal {0}", request.getUserPrincipal());
-            LOG.log(Level.INFO, "*** Is role admin {0}", request.isUserInRole("admin"));
-            LOG.log(Level.INFO, "*** Is role user {0}", request.isUserInRole("user"));
-          
-            externalContext.getSessionMap().put("user", u);  // Store User in session
+            lb.setLoggedIn(true);
             return "login-success";
         } catch (ServletException e) {
-              LOG.log(Level.INFO, "*** Login fail");
-              LOG.log(Level.INFO, "*** Error: " + e.getMessage());
-              e.printStackTrace();
+            LOG.log(Level.INFO, "*** Login fail");
+            LOG.log(Level.INFO, "*** Error: {0}", e.getMessage());
             FacesContext.getCurrentInstance().
-                    addMessage(null, 
+                    addMessage(null,
                             new FacesMessage(FacesMessage.SEVERITY_WARN,
                                     "Login Failed", null));
-            // Must set this (use the Flash-scope) else message
-            // wan't survive the redirect (see faces-config.xml)
             externalContext.getFlash().setKeepMessages(true);
         }
         return "login-fail";
     }
-    
+
     public String logout() {
         ExternalContext externalContext = FacesContext.getCurrentInstance().
                 getExternalContext();
@@ -81,5 +63,5 @@ public class LoginCtrl {
         LOG.log(Level.INFO, "*** Logout success");
         return "logout-success";
     }
-    
+
 }
