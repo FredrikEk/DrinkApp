ac767f39580dcb8a813c5a6150f43439a7f428bf
Martin <martinkastebo@gmail.com>
2014-10-23 16:02:15 +0200

    lade till funktion i Search för favourite..

--- DrinkApp/src/main/java/nu/drinkapp/ctrl/SearchDrinkCtrl.java
+++ DrinkApp/src/main/java/nu/drinkapp/ctrl/SearchDrinkCtrl.java
@@ -15,7 +15,7 @@ import nu.drinkapp.bb.DrinkSearchBB;
 import nu.drinkapp.bb.LoginBB;
 import nu.drinkapp.wrappers.IDrinkBook;
 import nu.drinkapp.wrappers.IRatingBook;
-import nu.drinkApp.wrappers.IFavouriteBook;
+import nu.drinkapp.wrappers.IFavouriteBook;
 import nu.drinkapp.wrappers.IUserBook;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,15 +29,14 @@ import javax.inject.Named;
 
 @Named
 @RequestScoped
-public class SearchDrinkCtrl{
+public class SearchDrinkCtrl {
 
     @Inject
     private Bar bar;
-    
     private DrinkSearchBB drinkSearchBB;
     private LoginBB loginBB;
     private static final Logger LOG = Logger.getLogger(AddDrinkCtrl.class.getName());
-    
+
     protected SearchDrinkCtrl() {
         // Must have for CDI
     }
@@ -56,7 +55,6 @@ public class SearchDrinkCtrl{
     //public IngredientListCtrl(SingletonShop shop) {
     //    this.pc = shop.getShop().getProductCatalogue();
     //}
-    
     @Inject
     public void setDrinkSearchBB(DrinkSearchBB drinkSearchBB) {
         this.drinkSearchBB = drinkSearchBB;
@@ -66,26 +64,26 @@ public class SearchDrinkCtrl{
     public void setLoginBB(LoginBB loginBB) {
         this.loginBB = loginBB;
     }
-    
+
     public void updateDrinks() {
         LOG.log(Level.INFO, "Testing" + drinkSearchBB.getSearchstring(), this);
-        if(drinkSearchBB != null && drinkSearchBB.getSearchstring() != null) {
+        if (drinkSearchBB != null && drinkSearchBB.getSearchstring() != null) {
             IDrinkBook db = bar.getDrinkBook();
             /*List<Drink> ld = db.searchByName(drinkSearchBB.getSearchstring());
-            List<DrinkBB> ldbb = new ArrayList();
-            for(Drink d : ld) {
-                ldbb.add(new DrinkBB(d));
-            }*/
+             List<DrinkBB> ldbb = new ArrayList();
+             for(Drink d : ld) {
+             ldbb.add(new DrinkBB(d));
+             }*/
             List<DrinkBB> ldbb = new ArrayList();
             LOG.log(Level.INFO, "Testing_drinkIngredients " + drinkSearchBB.getIngredients().toString(), this);
-            if(drinkSearchBB.getIngredients().isEmpty()) {
+            if (drinkSearchBB.getIngredients().isEmpty()) {
                 List<Drink> ld = db.searchByName(drinkSearchBB.getSearchstring());
-                for(Drink d : ld) {
+                for (Drink d : ld) {
                     ldbb.add(new DrinkBB(d));
                 }
             } else {
                 List<Object[]> lobj = db.searchByNameAndIngredient(drinkSearchBB.getSearchstring(), drinkSearchBB.getIngredients());
-                for(Object[] obj : lobj) {
+                for (Object[] obj : lobj) {
                     String drinkname = (String) obj[0];
                     String username = (String) obj[1];
                     Drink d = db.findByUsernameAndDrinkname(username, drinkname);
@@ -105,7 +103,7 @@ public class SearchDrinkCtrl{
 
     public void updateRating(int rate) {
         DrinkBB dbb = drinkSearchBB.getDrinkBB();
-        if(dbb != null && loginBB != null && loginBB.getUsername() != null && !loginBB.getUsername().equals("")) {
+        if (dbb != null && loginBB != null && loginBB.getUsername() != null && !loginBB.getUsername().equals("")) {
             IDrinkBook db = bar.getDrinkBook();
             IUserBook ub = bar.getUserBook();
             IRatingBook rb = bar.getRatingBook();
@@ -113,7 +111,7 @@ public class SearchDrinkCtrl{
             Drink d = db.findByUserAndDrinkname(drinkOwner, dbb.getDrinkname());
             User user = ub.findByName(loginBB.getUsername());
             Rating rating = rb.findByDrinkAndUser(d, user);
-            if(rating != null) {
+            if (rating != null) {
                 Rating updatedRating = new Rating(user, d, rate);
                 rb.update(updatedRating);
             } else {
@@ -125,11 +123,32 @@ public class SearchDrinkCtrl{
             }
         }
     }
-    
+
+    public void updateFavourite() {
+        DrinkBB dbb = drinkSearchBB.getDrinkBB();
+        if (dbb != null && loginBB != null && loginBB.getUsername() != null && !loginBB.getUsername().equals("")) {
+            IDrinkBook db = bar.getDrinkBook();
+            IUserBook ub = bar.getUserBook();
+            IFavouriteBook fb = bar.getFavouriteBook();
+            User drinkOwner = ub.findByName(dbb.getUsername());
+            Drink d = db.findByUserAndDrinkname(drinkOwner, dbb.getDrinkname());
+            User user = ub.findByName(loginBB.getUsername());
+            Favourite favourite = fb.findByDrinkAndUser(d, user);
+            if (favourite != null) {//If you click when it's already marke as favourite, you remove it...
+                fb.deleteByUserAndDrink(user, d);
+            } else {
+                LOG.log(Level.INFO, "user " + user.getUsername(), this);
+                LOG.log(Level.INFO, "drink " + drinkSearchBB.getDrinkBB().getDrinkname(), this);
+                Favourite newFavourite = new Favourite(user, d);
+                fb.create(newFavourite);
+            }
+        }
+    }
+
     public List<Integer> getEmptyStars() {
         DrinkBB dbb = drinkSearchBB.getDrinkBB();
         List<Integer> integerList = new ArrayList();
-        if(dbb != null && loginBB != null && loginBB.getUsername() != null && !loginBB.getUsername().equals("")) {
+        if (dbb != null && loginBB != null && loginBB.getUsername() != null && !loginBB.getUsername().equals("")) {
             IDrinkBook db = bar.getDrinkBook();
             IUserBook ub = bar.getUserBook();
             IRatingBook rb = bar.getRatingBook();
@@ -137,25 +156,25 @@ public class SearchDrinkCtrl{
             Drink d = db.findByUserAndDrinkname(drinkOwner, dbb.getDrinkname());
             User user = ub.findByName(loginBB.getUsername());
             Rating rating = rb.findByDrinkAndUser(d, user);
-            if(rating != null) {
+            if (rating != null) {
                 int noOfStars = 5;
-                for(int i = rating.getRating() + 1; i <= noOfStars; i++) {
+                for (int i = rating.getRating() + 1; i <= noOfStars; i++) {
                     integerList.add(i);
                 }
             } else {
                 int noOfStars = 5;
-                for(int i = 1; i <= noOfStars; i++) {
+                for (int i = 1; i <= noOfStars; i++) {
                     integerList.add(i);
                 }
             }
         }
         return integerList;
     }
-    
+
     public List<Integer> getFilledStars() {
         DrinkBB dbb = drinkSearchBB.getDrinkBB();
         List<Integer> integerList = new ArrayList();
-        if(dbb != null && loginBB != null && loginBB.getUsername() != null && !loginBB.getUsername().equals("")) {
+        if (dbb != null && loginBB != null && loginBB.getUsername() != null && !loginBB.getUsername().equals("")) {
             IDrinkBook db = bar.getDrinkBook();
             IUserBook ub = bar.getUserBook();
             IRatingBook rb = bar.getRatingBook();
@@ -163,26 +182,26 @@ public class SearchDrinkCtrl{
             Drink d = db.findByUserAndDrinkname(drinkOwner, dbb.getDrinkname());
             User user = ub.findByName(loginBB.getUsername());
             Rating rating = rb.findByDrinkAndUser(d, user);
-            if(rating != null) {
+            if (rating != null) {
                 int noOfStars = rating.getRating();
-                for(int i = 1; i <= noOfStars; i++) {
+                for (int i = 1; i <= noOfStars; i++) {
                     integerList.add(i);
                 }
-            } 
+            }
         }
         return integerList;
     }
-    
+
     public String getAvgRating() {
         DrinkBB dbb = drinkSearchBB.getDrinkBB();
-        if(dbb != null) {
+        if (dbb != null) {
             IDrinkBook db = bar.getDrinkBook();
             IUserBook ub = bar.getUserBook();
             IRatingBook rb = bar.getRatingBook();
             User drinkOwner = ub.findByName(dbb.getUsername());
             Drink d = db.findByUserAndDrinkname(drinkOwner, dbb.getDrinkname());
             Double avgRating = rb.getAverageRating(d);
-            if(avgRating < 0.5) {
+            if (avgRating < 0.5) {
                 return "Unrated";
             } else {
                 return "Avg (" + Double.toString(avgRating) + "/5)";
